06636af3d0985c029406b4f4a80f2370fde68099
Ken <kensong96@gmail.com>
2019-10-28 19:32:09 -0400

    delete item works fine

--- js/todos.js
+++ js/todos.js
@@ -3,3 +3,14 @@ $("li").click(function() {
     //使用if来转换
     $(this).toggleClass("completed");
 });
+
+
+//X button to delete the li
+$("span").click(function(event){
+    //alert("click on span");
+    $(this).parent().fadeOut(500,function () {
+        $(this).remove();//这里的两个this指的是不同的对象
+    });
+
+    event.stopPropagation();//停止event listener的连续唤醒
+});
