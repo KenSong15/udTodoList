d81419005a28c8a543e5a31e92bdb222f6ee7b9b
Ken <kensong96@gmail.com>
2019-10-28 19:47:44 -0400

    new item works fine

--- js/todos.js
+++ js/todos.js
@@ -1,16 +1,29 @@
 //check if i am clicking
-$("li").click(function() {
+//on只能被添加到运行时已经存在的部件上, 所以要指明一开始就存在的东西, 以及后面的触发目标
+$("ul").on("click","li",function() {
     //使用if来转换
     $(this).toggleClass("completed");
 });
 
 
 //X button to delete the li
-$("span").click(function(event){
+$("ul").on("click","span",function(event){
     //alert("click on span");
     $(this).parent().fadeOut(500,function () {
         $(this).remove();//这里的两个this指的是不同的对象
     });
 
     event.stopPropagation();//停止event listener的连续唤醒
+});
+
+//input field
+$("input[type='text']").keypress(function (event) {
+    //使用key code来检测按下的是什么键
+    if(event.which === 13){
+        //拿到输入的文字
+        let inStr = $(this).val();
+        $(this).val("");
+        //增加ul里面的HTML内容
+        $("ul").append("<li><span>X </span>" + inStr + "</li>")
+    }
 });
